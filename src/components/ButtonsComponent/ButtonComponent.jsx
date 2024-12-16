38dae5e68ce3fbd0b5423052c82ea0c2dfa74570
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2024-12-16 17:16:56 +0100

    Fixed some issues

--- src/components/ButtonsComponent/ButtonComponent.jsx
+++ src/components/ButtonsComponent/ButtonComponent.jsx
@@ -1,11 +1,15 @@
+//import { useState } from "react";
 function ButtonComponent({ array }) {
+
+
 
     const newArray = [...array]
 
     return (
+
         newArray.map((element) =>
             <>
-                < button key={element.id} type="button" className="btn btn-primary m-1" >{element.title}</ button>
+                < button key={element.id} type="button" className="btn btn-primary mx-1">{element.title}</ button >
             </>
         )
     )
